edca271e6cf86798b54bc0432472be88187651d7
Steve Wakhungu <stwakhungu@gmail.com>
2022-06-07 18:36:23 +0300

    commit tree b1d77fa8634aaa48324f94a7da1f8052ea85c374

---
+++ index.js
@@ -0,0 +1,8 @@
+const add = (parameter1, parameter2) => parameter1 + parameter2
+add(3,9)
+
+const divide = (parameter1, parameter2) => 2000 / 100
+divide(2000,100); //=> 20
+
+
+const square = (parameter1) => parameter1 * parameter1
